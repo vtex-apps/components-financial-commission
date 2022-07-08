25f005e63c8c372059d4c117236db1ab1e50c022
Erika Jissel Gutierrez Beltran <erika.beltran@vtex.com.br>
2022-07-08 11:33:33 -0500

    Remove console.info and improve the filter

--- react/components/ModalConfirm/index.tsx
+++ react/components/ModalConfirm/index.tsx
@@ -149,7 +149,6 @@ const ModalConfirm: FC<ModalConfirmData> = (props) => {
             <Alert
               type="warning"
               onClose={() => {
-                console.info('Close!!!!')
                 setAlertActive(true)
               }}
             >
